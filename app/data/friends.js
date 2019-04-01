77d2cf6be771afbc1cb87b86338d5330074b02b4
Thor Nolan <tnolan5764@gmail.com>
2019-03-31 19:43:04 -0700

    Set up skeleton of my survey page, working on client-side javascript for handling form submission.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,15 @@
 
+// Array for holding all of my friends data
+var friendsArray = [
 
 
 
 
-// Export my data array for use within my other files
-module.exports = friends;
+
+    
+]
+
+
+
+// Export my friends data array for use within my other files
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -15,7 +15,9 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./app/public"));
+
+// Provide server access to my public directory for serving my css and images
+app.use(express.static(path.join(__dirname, './app/public')));
 
 //=================================== ROUTES =============================================
 
